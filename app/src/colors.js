6695b158648aba3cb637da00f9ddb63d35abd580
irmabrr <barrios12214@gmail.com>
2025-08-13 22:21:25 -0400

    fixes

--- app/src/colors.js
+++ app/src/colors.js
@@ -1,124 +1,88 @@
 export const newpal = (id, title, colors, temp) => {
-    const palList = document.querySelector('.premade');
-    const pal  = document.createElement('ul');
 
-    pal.id = id;
-    pal.className = 'palette';
-    palList.appendChild(pal);
+  const palList = document.querySelector('.premade');
+  const pal = document.createElement('li');
 
-    const palContent = document.createElement('div');
-    palContent.className = 'pal-content';
-    pal.appendChild(palContent);
+  pal.id = id;
+  pal.className = 'palette';
+  palList.appendChild(pal);
 
-    const palTitle = document.createElement('h3');
-    palTitle.textContent = title;
-    palContent.appendChild(palTitle);
+  const palContent = document.createElement('div');
+  palContent.className = 'pal-content';
+  pal.appendChild(palContent);
 
-    // Color 1
-    const palColor1 = document.createElement('div');
-    palColor1.className = 'color-one';
-    palContent.appendChild(palColor1);
+  const palTitle = document.createElement('h3');
+  palTitle.textContent = title;
+  palContent.appendChild(palTitle);
 
-    const outerOne = document.createElement('div');
-    outerOne.className = 'outer-color-one';
-    palColor1.appendChild(outerOne);
+  colors.forEach((color) => {
+    const colorDiv = document.createElement('div');
+    colorDiv.className = 'color-block';
+    palContent.appendChild(colorDiv);
 
-    const boxOne = document.createElement('div');
-    boxOne.className = 'box-color-one';
-    boxOne.style.backgroundColor = colors[0];
-    outerOne.appendChild(boxOne);
+    const outer = document.createElement('div');
+    outer.className = 'outer-color';
+    colorDiv.appendChild(outer);
 
-    const textOne = document.createElement('div');
-    textOne.className = 'text-color-one';
-    boxOne.appendChild(textOne);
+    const box = document.createElement('div');
+    box.className = 'color-box';
+    box.dataset.hex = color;
+    box.style.backgroundColor = color;
+    outer.appendChild(box);
 
-    const firstText =  document.createElement('p');
-    firstText.className = 'first-text';
-    firstText.textContent = 'Text Color 1';
-    textOne.appendChild(firstText);
+    const whiteDiv = document.createElement('div');
+    whiteDiv.className = 'white-block';
+    const whiteText = document.createElement('p');
+    whiteText.className = 'white';
+    whiteText.textContent = 'Text Color 1';
+    whiteDiv.appendChild(whiteText);
+    box.appendChild(whiteDiv);
 
-    const nextText = document.createElement('p');
-    nextText.className = 'two-text';
-    nextText.textContent = 'Text Color 2';
-    textOne.appendChild(nextText);
+    const blackDiv = document.createElement('div');
+    blackDiv.className = 'black-block';
+    const blackText = document.createElement('p');
+    blackText.className = 'black';
+    blackText.textContent = 'Text Color 2';
+    blackDiv.appendChild(blackText);
+    box.appendChild(blackDiv);
 
-    // Color 2
-    const palColor2 = document.createElement('div');
-    palColor2.className = 'color-two';
-    palContent.appendChild(palColor2);
+    const copybutton = document.createElement('button');
+    copybutton.className = 'copy-button';
+    copybutton.textContent = `Copy ${color}`;
+    box.appendChild(copybutton);
 
-    const outerTwo = document.createElement('div');
-    outerTwo.className = 'outer-color-two';
-    palColor2.appendChild(outerTwo);
-
-    const boxTwo = document.createElement('div');
-    boxTwo.className = 'box-color-two';
-    boxTwo.style.backgroundColor = colors[1];
-    outerTwo.appendChild(boxTwo);
-
-    const textTwo = document.createElement('div');
-    textTwo.className = 'text-color-two';
-    boxTwo.appendChild(textTwo);
-
-    const secondText =  document.createElement('p');
-    secondText.className = 'first-text';
-    secondText.textContent = 'Text Color 1';
-    textTwo.appendChild(secondText);
-
-    const twoText = document.createElement('p');
-    twoText.className = 'two-text';
-    twoText.textContent = 'Text Color 2';
-    textTwo.appendChild(twoText);
-
-    // Color 3
-    const palColor3 = document.createElement('div');
-    palColor3.className = 'color-three';
-    palContent.appendChild(palColor3);
-
-    const outerThree = document.createElement('div');
-    outerThree.className = 'outer-color-three';
-    palColor3.appendChild(outerThree);
-
-    const boxThree = document.createElement('div');
-    boxThree.className = 'box-color-three';
-    boxThree.style.backgroundColor = colors[2];
-    outerThree.appendChild(boxThree);
-
-    const textThree = document.createElement('div');
-    textThree.className = 'text-color-tree';
-    boxThree.appendChild(textThree);
-
-    const thirdText =  document.createElement('p');
-    thirdText.className = 'third-text';
-    thirdText.textContent = 'Text Color 1';
-    textThree.appendChild(thirdText);
-
-    const afterText = document.createElement('p');
-    afterText.className = 'two-text';
-    afterText.textContent = 'Text Color 2';
-    textThree.appendChild(afterText);
-
-    const deleteButt = document.createElement('button');
-    deleteButt.className = 'delete-butt';
-    deleteButt.textContent = 'Delete Palette';
-    palContent.appendChild(deleteButt);
-
-    deleteButt.addEventListener('click', () => {
-      pal.remove();
+    copybutton.addEventListener('click', () => {
+      navigator.clipboard.writeText(color).then(() => {
+        const originalText = copybutton.textContent;
+        copybutton.textContent = 'Copied hex code!';
+        setTimeout(() => (copybutton.textContent = originalText), 1000);
+      });
     });
+  });
 
-    const tempText = document.createElement('div');
-    tempText.className = 'temp-text';
-    tempText.textContent = temp ;
-    if (temp === 'warm') {
-      tempText.style.backgroundColor = 'red';
-    } else if (temp === 'cool') {
-      tempText.style.backgroundColor = 'blue';
-    } else {
-      tempText.style.backgroundColor = 'gray';
-    }
-    palContent.appendChild(tempText);
-}
+  const deleteButt = document.createElement('button');
+  deleteButt.className = 'delete-butt';
+  deleteButt.textContent = 'Delete Palette';
+  palContent.appendChild(deleteButt);
+
+  deleteButt.addEventListener('click', () => {
+    pal.remove();
+  });
+
+  const tempText = document.createElement('div');
+  tempText.className = 'temp-text';
+   if (temp === 'warm') {
+        tempText.style.backgroundColor = 'red';
+        tempText.textContent = 'Warm';
+      } else if (temp === 'cool') {
+        tempText.style.backgroundColor = 'blue';
+        tempText.textContent = 'Cool';
+      } else {
+        tempText.style.backgroundColor = 'gray';
+        tempText.textContent = 'Neutral';
+      }
+  palContent.appendChild(tempText);
+};
 
 export const handleFormSubmit = (event) => {
   event.preventDefault();
@@ -135,8 +99,7 @@ export const handleFormSubmit = (event) => {
   }
 
   const id = 'pal-' + Math.random().toString(36);
-
   newpal(id, PalTitle, [color1, color2, color3], temp);
-  
+
   event.target.reset();
-}
+};
